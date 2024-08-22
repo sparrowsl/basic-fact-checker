3492e7c574eeb0cdca8a76bf7d074aee64efab2d
sparrowsl <benthorpe.dev@gmail.com>
2024-08-22 14:28:54 +0000

    fix: add validation check for login, check hashed password, add payload to jwt

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,27 +1,58 @@
 import db from "$lib/prisma.js";
 import { fail, redirect } from "@sveltejs/kit";
-import { toast } from "svelte-sonner";
+import * as v from "valibot";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET_KEY } from "$env/static/private";
+
+const loginSchema = v.object({
+  username: v.pipe(
+    v.string(),
+    v.trim(),
+    v.minLength(2, "username must be 2 or more letters"),
+  ),
+  password: v.pipe(
+    v.string(),
+    v.minLength(4, "password must be 4 or more characters"),
+  ),
+});
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-	default: async ({ request, cookies }) => {
-		// Get the data from the form
-		const form = /** @type {{username: string, password: string}} */ (
-			Object.fromEntries(await request.formData())
-		);
+  default: async ({ request, cookies }) => {
+    // Get the data from the form
+    const form = /** @type {{username: string, password: string}} */ (
+      Object.fromEntries(await request.formData())
+    );
 
-		// check if the user exists in the database
-		const user = await db.user.findUnique({
-			where: { username: form.username },
-		});
-		// TODO: hash password using bcrypt or argon
-		if (!user || user.password !== form.password) {
-			return fail(400, { message: "Invalid username or password!!" });
-		}
+    // validate the incoming data
+    const { output, issues, success } = v.safeParse(loginSchema, form, {
+      abortEarly: true,
+    });
+    if (!success) {
+      const errors = issues.map((issue) => issue.message);
+      return fail(400, { message: errors[0] });
+    }
 
-		// TODO: remove the password from the token
-		cookies.set("user", JSON.stringify(user), { path: "/" });
-		toast.success("successfully logged in");
-		redirect(307, "/");
-	},
+    // check if the user exists in the database
+    const user = await db.user.findUnique({
+      where: { username: output.username },
+    });
+
+    try {
+      // if no user found or password does not match then return error message
+      if (!user || (await bcrypt.compare(output.password, user?.password))) {
+        return fail(400, { message: "Invalid username or password!!" });
+      }
+
+      const { password, ...rest } = user;
+      const payload = jwt.sign(rest, JWT_SECRET_KEY); // send payload to JWT
+
+      cookies.set("user", payload, { path: "/", secure: true });
+    } catch (/** @type {*} */ _e) {
+      return fail(400, { message: _e.message });
+    }
+
+    redirect(307, "/");
+  },
 };
